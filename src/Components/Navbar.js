dbc91787a618083e814128f02a50ba6b1f9439a5
demi.coder <104001201+OgaDavid@users.noreply.github.com>
2022-09-19 16:35:25 -0700

    Edited Readme

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <div className="navbar">
+                
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
