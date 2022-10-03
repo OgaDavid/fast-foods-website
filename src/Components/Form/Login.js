d9bdb1b5f1f6314e87fe7a4e9442c49616eec764
demi.coder <104001201+OgaDavid@users.noreply.github.com>
2022-10-03 15:11:14 -0700

    Finished refactoring Login Form

--- src/Components/Form/Login.js
+++ src/Components/Form/Login.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import Logo from '../../utils/Logo';
 import './form.css';
+import loginData from '../../Data/LoginData';
+import Input from '../../utils/Input';
 
 const Login = () => {
+
+    function input(input) {
+        return(
+            <Input
+            key={input.id}            
+            label={input.label}
+            placeholder={input.placeholder}
+            type={input.type}
+            htmlFor={input.htmlFor}
+            />
+        )
+    }
     return(
         <div className='login-container'>
             <Logo/>
             <h1 className="heading">👌Welcome Back!</h1>
             <form action="submit">
-                <div className="field">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" placeholder='Your Email'/>
-                </div>
-                <div className="field">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder='Enter password'/>
-                </div>
+                {loginData.map(input)}
                 <div className="register-cta">
-                <button type='submit'>Login</button>
-                <p><a href='/'>Don't have an account? SIGN UP</a></p>
+                    <button type='submit'>Login</button>
+                    <p><a href='/'>Don't have an account? SIGN UP</a></p>
                 </div>
             </form>
         </div>
---
+++ src/Data/LoginData.js
@@ -0,0 +1,18 @@
+const loginData = [
+    {
+        id: 1,
+        label: "Email",
+        placeholder: "Your Email",
+        htmlFor: "email",
+        type: "email"
+    },
+    {
+        id: 2,
+        label: "Password",
+        placeholder: "Password",
+        htmlFor: "password",
+        type: "password"
+    }
+]
+
+export default loginData;
