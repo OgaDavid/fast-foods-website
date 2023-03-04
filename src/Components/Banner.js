9c522210c71b05042b969c7fffdb29e0f9e65c46
David oga <104001201+OgaDavid@users.noreply.github.com>
2023-03-04 14:35:59 -0800

    Update Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className="banner" id="banner">
-        <p>🚀 Checkout our latest categories ranging from veggies to healthy fruits!</p>
+        <p>🚀 Checkout our latest categories ranging from veggies to really healthy fruits!</p>
     </div>
   )
 }
